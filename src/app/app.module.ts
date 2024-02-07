16581241283eba506a48595aa4be864cd2f955ce
Peter Dickten <peter.dickten@dcs-fuerth.de>
2024-02-07 12:02:05 +0100

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { BookListComponent } from './books/book-list/book-list.component';
 import { FormsModule } from '@angular/forms';
 import { CalculatorComponent } from './shared/calculator/calculator.component';
+import { BookFilterPipe } from './books/book-filter.pipe';
+import { RatingComponent } from './shared/rating/rating.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BookListComponent,
-    CalculatorComponent
+    CalculatorComponent,
+    BookFilterPipe,
+    RatingComponent
   ],
   imports: [
     BrowserModule,
--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -1,10 +1,11 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges, ViewEncapsulation } from '@angular/core';
+import { Book } from '../book.interface';
 
 @Component({
   selector: 'book-list',
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.css'})
-export class BookListComponent {
+export class BookListComponent implements OnInit, OnChanges {
 
   filterValue: string = '';
 
@@ -12,23 +13,40 @@ export class BookListComponent {
 
   breite = 50;
 
-  books = [
+  books: Book[] = [
     {
       title: 'Angular 16',
       isbn: '123456',
       price: 9.99,
-      coverUrl: "https://m.media-amazon.com/images/I/71rUdxFkaCL._AC_UY218_.jpg"
+      coverUrl: "https://m.media-amazon.com/images/I/71rUdxFkaCL._AC_UY218_.jpg",
+      rating: 4.5
     },
     {
       title: 'Angular 17 is great',
       isbn: '123457',
       price: 17.99,
-      coverUrl: "https://m.media-amazon.com/images/I/51zVmluMLCL._AC_UY218_.jpg"
+      coverUrl: "https://m.media-amazon.com/images/I/51zVmluMLCL._AC_UY218_.jpg",
+      rating: 4.7
       }
   ];
 
   toggleCover() {
     this.coverIsVisible = !this.coverIsVisible;
+  }
+    
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges', changes);
+  }
+
+  ngOnInit(): void {  
+  }
+  upRating($event: string) {
+    console.log('upRating', $event);
+
+    const book = this.books.find(book => book.isbn === $event);
+    if (book) {
+      book.rating = Math.min(5, parseFloat((book.rating + 0.1).toFixed(2)));
+    }
   }
     
 }
