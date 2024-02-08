99fefb9000f0f0196b60877f36beb398e71941d6
Peter Dickten <peter.dickten@dcs-fuerth.de>
2024-02-08 21:21:45 +0100

    WIP

--- src/app/books/book-data.service.spec.ts
+++ src/app/books/book-data.service.spec.ts
@@ -1,16 +1,27 @@
-import { TestBed } from '@angular/core/testing';
 
+import { of } from 'rxjs';
 import { BookDataService } from './book-data.service';
 
 describe('BookDataService', () => {
-  let service: BookDataService;
 
+  let bookDataService: BookDataService;
+  let mockHttp: any;
+  
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    // service = TestBed.inject(BookDataService);
+    // TestBed.configureTestingModule({});
+    mockHttp = jasmine.createSpyObj('mockHttp', ['get']);
+    bookDataService = new BookDataService(mockHttp);
   });
 
   it('should be created', () => {
-    // expect(service).toBeTruthy();
+    expect(bookDataService).toBeTruthy();
   });
+
+  it('should load an existing book using the correct URL', () => {
+    const book = { isbn: '42', name: 'Peters Angular-Kurs' };
+    mockHttp.get.and.returnValue(of(book));
+    bookDataService.getBook('42');
+    expect(mockHttp.get).toHaveBeenCalledWith('http://localhost:3000/books/42');
+  })
 });
+
