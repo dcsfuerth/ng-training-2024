f670bcd106efe09f60f323f5b761792cba166f83
Peter Dickten <peter.dickten@dcs-fuerth.de>
2024-02-09 11:12:55 +0100

    WIP

--- code/books/src/app/helpers.spec.ts
+++ code/books/src/app/helpers.spec.ts
@@ -1,14 +1,15 @@
 import { add } from "./helpers";
 
 describe('helper.ts', () => {
-    //
-    it('should add to number', () => {
+    
+    it('should add two numbers', () => {
         const result = add(1, 2);
         expect(result).toEqual(3);
     });
 
-    it('should add two string', () => {
-        expect("1" + "2").toEqual("12");
+    it('should add two numbers with 0', () => {
+        const result = add(1, 2) + 0;
+        expect(result).toEqual(3);
     });
-        
+
 })
--- code/redux/src/app/app.module.ts
+++ code/redux/src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { counterReducer } from './counter';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { counterReducer } from './counter';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    StoreModule.forRoot({count: counterReducer})
+    StoreModule.forRoot({count: counterReducer}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })
   ],
   providers: [],
   bootstrap: [AppComponent]
