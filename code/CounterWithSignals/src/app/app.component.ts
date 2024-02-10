4d7d351b158c081e50c7901d09ef4f32ecdbf604
Andreas Klapproth <33018847+silazzz@users.noreply.github.com>
2024-02-10 20:57:20 +0100

    FIX app.component.ts
    fixed missing provider

--- code/CounterWithSignals/src/app/app.component.ts
+++ code/CounterWithSignals/src/app/app.component.ts
@@ -21,6 +21,7 @@ export const CounterStore = signalStore(
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
+  providers: [CounterStore],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
